726ea66ffc835fdbb2ee560467d0bcbf15a9ef31
yousra Jaradh <yousrakhaleel@gmail.com>
2020-12-04 10:01:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+var item=
+[
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu":0.78,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu":0.26,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+]
+
+function topping(){
+    for(let l=0;l<item.length;l++){
+        var topping= item[l].topping;
+        for(let i=0;i<topping.length;i++){
+            console.log(topping[i].type)
+        }
+    }
+}
+function batter(){
+    for(let l=0;l<item.length;l++){
+        var topping= item[l].batters;
+        for(let i=0;i<batters.length;i++){
+            console.log(batters[i].type)
+        }
+    }
+}
+function sum(){
+    let sum=0
+    for(let l=0;l<item.length;l++){
+        var ppu= item[l].ppu;
+        sum= sum+ppu;
+    }
+   console.log(sum);
+}
